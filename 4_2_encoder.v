66f449cd55456c861310a371fec9bd090f17cd87
Deepam kumar Bisoyi <127032868+deepam22-oct@users.noreply.github.com>
2026-04-16 20:55:03 +0530

    Add 4-to-2 encoder using behavioral and dataflow modeling

---
+++ 4_2_encoder.v
@@ -0,0 +1,31 @@
+// Using Behavioral Modeling
+
+module 4_2_encoder (
+  input [3:0]in ,
+  output reg [1:0] out 
+);
+
+  always(*) begin 
+    case(in) 
+      4'b0001 : out = 2'b00 ;
+      4'b0010 : out = 2'b01 ;
+      4'b0100 : out = 2'b10 ;
+      4'b1000 : out = 2'b11 ;
+    endcase
+  end
+endmodule
+
+// Dataflow Modeling
+
+module encoder_4_2_dataflow (
+  input [3:0] in,
+  output [1:0] out
+);
+
+assign out[1] = in[2] | in[3];
+assign out[0] = in[1] | in[3];
+
+endmodule
+
+
+
